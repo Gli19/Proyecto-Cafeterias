e5b04eef654f74da4f9e8c01a2da4b21264e9c6c
luis <luis.16trejo@gmail.com>
2024-02-14 10:35:03 -0600

    eliminar

--- BD_cafeteria/administrador.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM proyecto_cafeterias.administrador
-LIMIT 0, 1000
-
--- Date: 2024-01-09 23:23
-*/
-INSERT INTO `` (`id`,`contrasena`,`nombre_usuario`) VALUES (1,'contra','ERICK');
-INSERT INTO `` (`id`,`contrasena`,`nombre_usuario`) VALUES (2,'contra2','LUIS');
-INSERT INTO `` (`id`,`contrasena`,`nombre_usuario`) VALUES (3,'contra3','MARCOS');
